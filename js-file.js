c18a39f1079cef2e0514b3693efcf1bf0d204c5d
1c3T1m3 <200052183+1c3T1m3@users.noreply.github.com>
2025-05-02 11:46:55 +0300

    You can draw with hover, you can't choose 'pixels' of grid.

--- js-file.js
+++ js-file.js
@@ -1,18 +1,28 @@
 const container = document.querySelector("#container");
-
+let unique = 0;
 //This for loop creates an array of 16 'row' divs.
 for (let i = 0; i < 16; i++) {
 
     const rows = document.createElement("div");
-    rows.id ="rows";
+    rows.classList.add("rows");
     //rows.textContent = i;
 
     container.appendChild(rows);
     for (let j = 0; j < 16; j++){
-        const kouti = document.createElement("div");
-        kouti.id ="box";
-        kouti.textContent = "seira "+i+" kouti "+j;
-
-        rows.appendChild(kouti);
+        const colum = document.createElement("div");
+        colum.id = `b${unique}`;
+        colum.classList.add("column");
+        colum.textContent = " . ";
+        unique++;
+        rows.appendChild(colum);
     }
   }
+
+for (let i = 0; i < unique; i++) {
+
+  const element = document.querySelector(`#b${i}`);
+
+  element.addEventListener('mouseenter', () => {
+    element.style.backgroundColor = 'black';
+  });
+}
